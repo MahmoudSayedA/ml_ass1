ddc7a8cc8509eb7223a815dc4974a3bfbff5b714
Mahmoud Sayed <mahmoudsayed1332002@gmail.com>
2023-11-14 15:14:42 +0200

    build and test logistic reg model

--- task.py
+++ task.py
@@ -41,7 +41,7 @@ y_loan_status = loan_old_data["Loan_Status"]
 
 # iii) Shuffle and split the data into training and testing sets
 X_train, X_test, y_train_loan_amount, y_test_loan_amount, y_train_loan_status, y_test_loan_status = train_test_split(
-    X, y_loan_amount, y_loan_status, test_size=0.3, random_state=42)
+    X, y_loan_amount, y_loan_status, test_size=0.3, random_state=22)
 
 # # test
 # print(len(X_train), len(X_test))
@@ -75,12 +75,88 @@ linear_reg_model.fit(X_train_encoded, y_train_loan_amount)
 # step-five Evaluate the linear regression model using R-squared score
 y_pred_loan_amount = linear_reg_model.predict(X_test_encoded)
 r2 = r2_score(y_test_loan_amount, y_pred_loan_amount)
-print("R^squared Score for Linear Regression:", r2)
+print(f'{"#"*50} R^squared Score for Linear Regression: {r2}')
+
+
+
+def sigmoid(x):
+    x = np.float64(x)
+    return (1/(1+np.exp(-1*x)))
+
+class LogisticRegression:
+
+    def __init__(self, lr = 0.01, iterations = 3000):
+        self.learning_rate = lr
+        self.iterations = iterations
+        self.thetas = None
+        self.theta0 = None
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        m_sample, n_features = X.shape
+        self.thetas = np.zeros(n_features)
+        self.theta0 = 0
+
+        for _ in range(self.iterations):
+            linear_prediction = np.dot(X, self.thetas) + self.theta0
+            prediction = sigmoid(linear_prediction)
+
+            d_thetas = (1 / m_sample) * np.dot(X.T, (prediction - y))
+            d_theta0 = (1 / m_sample) * np.sum(prediction - y)
+            
+            self.thetas -= self.learning_rate * d_thetas.astype(float)
+            self.theta0 -= self.learning_rate * d_theta0
+
+    def predict(self, X):
+        linear_pred = np.dot(X, self.thetas) + self.theta0
+        y_pred = sigmoid(linear_pred)
+        class_pred = [0 if y < 0.5 else 1 for y in y_pred]
+        return class_pred
+
+
+# Now i will test logistic_reg. method
+logistic_reg_model = LogisticRegression()
+logistic_reg_model.fit(X_train_encoded, y_train_loan_status)
+
+# g) Function to calculate accuracy from scratch
+def calculate_accuracy(y_pred, y):
+    accuracy = np.mean(y_pred == y)
+    return accuracy
+
+
+# Calculate accuracy on the test set
+y_pred = logistic_reg_model.predict(X_test_encoded)
+accuracy_test = calculate_accuracy(y_pred, y_test_loan_status)
+print(f'{"#"*50} Test Accuracy for Logistic Regression: {accuracy_test * 100 } %')
+
+
+# test loan new data
+new_loan_data = pd.read_csv("loan_new.csv")
+# clear records contain null values
+new_loan_data = new_loan_data.dropna()
+
+# drop non critical measures
+new_X = new_loan_data.drop(columns=["Loan_ID"])
+
+# normalize the new data
+new_X_encoded = pd.get_dummies(new_X, columns=["Gender", "Married", "Dependents", "Education", "Credit_History", "Property_Area"], drop_first=True)
+new_X_encoded[numerical_columns] = (new_X_encoded[numerical_columns] - mean_values) / std_values
+
+# predict loan amount and status
+loan_amount_prediction = linear_reg_model.predict(new_X_encoded)
+loan_status_prediction = logistic_reg_model.predict(new_X_encoded)
+
+# combine predicted values with the sheet
+new_loan_data["loan_amount_prediction"] = loan_amount_prediction
+new_loan_data["loan_status_prediction"] = loan_status_prediction
+
+print("new loan data: \n" + new_loan_data.to_string())
+
+
 
 
 # there are a problem with sigmoid method with the datatype of (x) .. take a look of it
 
-# # f) Fit a logistic regression model from scratch using gradient descent
+# f) Fit a logistic regression model from scratch using gradient descent
 # def sigmoid(x):
 #     try:
 #         return 1 / (1 + np.exp(-x))
@@ -99,24 +175,3 @@ print("R^squared Score for Linear Regression:", r2)
 #         theta -= learning_rate * gradient
     
 #     return theta
-
-# # Now i will test logistic_reg. method
-# learning_rate = 0.01
-# num_iterations = 1000
-# theta_loan_status = logistic_regression(X_train_encoded, y_train_loan_status, learning_rate, num_iterations)
-
-# # g) Function to calculate accuracy from scratch
-# def calculate_accuracy(X, y, theta):
-#     X = np.column_stack((np.ones((X.shape[0], 1)), X))
-#     z = np.dot(X, theta)
-#     h = sigmoid(z)
-#     predicted_labels = int(h >= 0.5)
-#     accuracy = np.mean(predicted_labels == y)
-#     return accuracy
-
-
-# # Calculate accuracy on the test set
-# X_test_with_intercept = np.column_stack((np.ones((X_test_encoded.shape[0], 1)), X_test_encoded))
-# accuracy_test = calculate_accuracy(X_test_encoded, y_test_loan_status, theta_loan_status)
-# print("Test Accuracy for Logistic Regression:", accuracy_test)
-
