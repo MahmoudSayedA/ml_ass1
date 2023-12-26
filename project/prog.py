fe72e5617bb3dc06e1545f73332436b144af57af
Nour Okbi <nourokbi1@gmail.com>
2023-12-26 13:42:11 +0200

    edit reshaping

--- project/prog.py
+++ project/prog.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import MinMaxScaler
 from skimage.transform import resize
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 # Load the dataset from CSV
 mnist_train_data = pd.read_csv("mnist_train.csv")
@@ -32,31 +32,32 @@ normalized_pixels = pixels / 255.0
 
 # Resize images to dimensions of 28 by 28
 # a collection of matrices every matrix represents an image 28x28
-# convert it to a 1D array to split and use it in the ML model
-resized_pixels = np.array([np.array(pixel).reshape(28, 28).flatten() for pixel in normalized_pixels.values])
+resized_pixels = normalized_pixels.values.reshape(-1, 28, 28)
+
 
 # Visualize some resized images
-num_samples = 5
+plt.figure(figsize=(10, 2))
+num_samples = 10
 for i in range(num_samples):
     plt.subplot(1, num_samples, i + 1)
-    plt.imshow(resized_pixels[i].reshape(28, 28), cmap='gray')
+    plt.imshow(resized_pixels[i], cmap='gray')
     plt.title(f"Label: {labels[i]}")
     plt.axis('off')
 plt.show()
 
 # Split the training data into training and validation sets
 x_train, x_val, y_train, y_val = train_test_split(
-    resized_pixels, labels, test_size=0.2, random_state=562)
+    normalized_pixels, labels, test_size=0.2, random_state=52)
 
 # Create a K-NN classifier
 knn = KNeighborsClassifier()
 
 # Define the hyperparameter grid to search
-param_grid = {'n_neighbors': [3, 5, 7], 'weights': ['uniform', 'distance']}
+param_grid = {'n_neighbors': [3, 5], 'weights': ['uniform', 'distance']}
 
 #! grid search takes amout of time to end..
 # Perform grid search using cross-validation
-grid_search = GridSearchCV(knn, param_grid, cv=5, scoring='accuracy')
+grid_search = GridSearchCV(knn, param_grid, cv=3, scoring='accuracy')
 grid_search.fit(x_train, y_train)
 
 # Get the best hyperparameters from the grid search
@@ -65,13 +66,16 @@ print("Best Hyperparameters:", best_params)
 
 # Train the K-NN classifier with the best hyperparameters on the training set
 best_knn = KNeighborsClassifier(n_neighbors=best_params['n_neighbors'], weights=best_params['weights'])
-#! this is best params from grid search
+# ! this is best params from grid search
 # best_knn = KNeighborsClassifier(n_neighbors=3, weights='distance')
 best_knn.fit(x_train, y_train)
 
 # Predictions on the validation set
-y_val_pred = best_knn.predict(x_val)
+y_preddiction = best_knn.predict(x_val)
 
 # Evaluate the performance on the validation set
-accuracy = accuracy_score(y_val, y_val_pred)
+accuracy = accuracy_score(y_val, y_preddiction)
+knn_confusion_matrix = confusion_matrix(y_val, y_preddiction)
+
 print(f"Validation Accuracy: {accuracy*100:.3f}%")
+print("Confusion Matrix (K-NN):\n", knn_confusion_matrix )
